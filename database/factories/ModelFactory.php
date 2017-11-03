279086056f75262caab0c643c7e144396a1f5a2d
Enner <eriec.42@gmail.com>
2017-11-02 22:11:03 -0600

    Unit Testing Presentation Logic

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,6 +12,10 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Concert;
+use Carbon\Carbon;
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
@@ -20,5 +24,20 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Concert::class, function (Faker\Generator $faker) {
+    return [
+        'title' => 'Example Band',
+        'subtitle' => 'with The Fake Openers',
+        'date' => Carbon::parse('+2 weeks'),
+        'ticket_price' => 2000,
+        'venue' => 'The Example Theatre',
+        'venue_address' => '123 Example Lane',
+        'city' => 'Fakeville',
+        'state' => 'ON',
+        'zip' => '90210',
+        'additional_information' => 'Some sample additional information.'
     ];
 });
